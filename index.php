d47b18ca1dce357df46f48a292e839ad8bd64943
Leo Mancini <mancini@leo.gd>
2021-12-31 21:57:21 -0500

    Updating indentation

--- index.php
+++ index.php
@@ -1,19 +1,19 @@
 <!DOCTYPE HTML>
 <html>
-	<head>
-		<title>Every Day</title>
-		<link rel='stylesheet/less' href='resources/css/style.less?nocache=132543657'>
-		<script src='//cdnjs.cloudflare.com/ajax/libs/less.js/3.11.1/less.min.js'></script>
-		<meta name='viewport' content='width=device-width, initial-scale=1'>
-	</head>
-	<body>
-		<div id='time'></div>
-		<div id='controls'>
-			<div id='sliders'>
-				<div class='sliderOutputPair'>Hour: <span class='output' id='hour'></span><input type='range' min='00' max='23' value='12' class='slider' id='hour'></div>
-				<div class='sliderOutputPair'>Minute: <span class='output' id='minute'></span><input type='range' min='00' max='59' value='00' class='slider' id='minute'></div>
-			</div>
-			<div id='buttons'>
+    <head>
+        <title>Every Day</title>
+        <link rel='stylesheet/less' href='resources/css/style.less?nocache=132543657'>
+        <script src='//cdnjs.cloudflare.com/ajax/libs/less.js/3.11.1/less.min.js'></script>
+        <meta name='viewport' content='width=device-width, initial-scale=1'>
+    </head>
+    <body>
+        <div id='time'></div>
+        <div id='controls'>
+            <div id='sliders'>
+                <div class='sliderOutputPair'>Hour: <span class='output' id='hour'></span><input type='range' min='00' max='23' value='12' class='slider' id='hour'></div>
+                <div class='sliderOutputPair'>Minute: <span class='output' id='minute'></span><input type='range' min='00' max='59' value='00' class='slider' id='minute'></div>
+            </div>
+            <div id='buttons'>
                 <div id='buttonsSet'>
                     <button id='sunrise'>Sunrise</button>
                     <button id='civil_twilight_begin'>Twilight AM</button>
@@ -22,10 +22,10 @@
                     <button id='sunset'>Sunset</button>
                     <small>Powered by <a href='https://sunrise-sunset.org/api' target='_blank'>Sunrise-Sunset API</a></small>
                 </div>
-			</div>
-		</div>
-		<div id='spinner'></div>
-		<div id='images'></div>
-		<script src='resources/js/main.js'></script>
-	</body>
+            </div>
+        </div>
+        <div id='spinner'></div>
+        <div id='images'></div>
+        <script src='resources/js/main.js'></script>
+    </body>
 </html>
